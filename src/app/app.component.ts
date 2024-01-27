1699c8729c207b3b7625c2b464706b1d6b082f94
Mohit Gupta <gmohit5454@gmail.com>
2024-01-27 20:53:36 +0530

    added word-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+  words = '';
+  limit = 10;
+
+  handleRandomNumber(){
+    this.limit = Math.random()*10;
+  }
+
+  genrate(){
+    this.words = arrayWords.slice(0, this.limit).join(" ");
+  }
+
+
+
 }
